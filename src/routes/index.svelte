b1b4fe5d2800b317de3af734c5169c0b6c5f1afb
Rafael <rafael.martinezcalvo@orange.fr>
2022-04-13 21:16:28 +0200

    Add sessions link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,6 +29,14 @@
 						S'inscrire
 					</a>
 				</div>
+				<div class="ml-3 inline-flex rounded-md shadow">
+					<a
+						href="/sessions"
+						class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+					>
+						Sessions
+					</a>
+				</div>
 			</div>
 		</div>
 		<p>
