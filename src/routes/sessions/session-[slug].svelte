f3c293cd6c26553e209864b632ac18010bfdb25c
Rafael <rafael.martinezcalvo@orange.fr>
2022-04-13 21:31:08 +0200

    add back button in sessions

--- src/routes/sessions/session-[slug].svelte
+++ src/routes/sessions/session-[slug].svelte
@@ -22,6 +22,7 @@
 <script lang="ts">
 	import ListItem from "$lib/components/sessions/ListItem.svelte";
 	import type { IPostFull } from "$lib/types/post";
+	import Icon from "@iconify/svelte";
 
 	export let post: IPostFull;
 	let Renderer = post.renderer;
@@ -36,7 +37,7 @@
 	/>
 </svelte:head>
 
-<div class="mx-auto max-w-5xl min-h-screen bg-white border border-gray-200">
+<div class="main flex flex-col mx-auto max-w-5xl bg-white border border-gray-200">
 	<div class="border-b border-b-gray-200">
 		<ListItem
 			title={post.title}
@@ -50,11 +51,19 @@
 	<div class="markdown-body py-6 px-8">
 		<Renderer />
 	</div>
+	<div class="grow" />
+	<a href="/sessions" class="mx-auto my-4 w-max flex items-center gap-2 uppercase font-semibold">
+		<Icon icon="heroicons-solid:arrow-narrow-left" />
+		retour
+	</a>
 </div>
 
 <style global>
 	/* Disable tailwind's css reset */
 	.markdown-body * {
 		all: revert;
+	}
+	.main {
+		min-height: calc(100vh - 64px);
 	}
 </style>
